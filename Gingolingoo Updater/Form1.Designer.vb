5db9fed981cc7e5eced822c033be965c71cae59f
Onkelz-Freak1993 <Onkelz-Freak1993@hotmail.de>
2018-11-14 16:50:17 +0100

    Updater geupdated :'D

--- Gingolingoo Updater/Form1.Designer.vb
+++ Gingolingoo Updater/Form1.Designer.vb
@@ -29,11 +29,15 @@ Partial Class Form1
         Me.UpdateVersion = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.Updateprogress = New System.Windows.Forms.ProgressBar()
+        Me.UpdateBW = New System.ComponentModel.BackgroundWorker()
         Me.SuspendLayout()
         '
         'AppVersion
         '
         Me.AppVersion.AutoSize = True
+        Me.AppVersion.BackColor = System.Drawing.Color.Transparent
+        Me.AppVersion.ForeColor = System.Drawing.Color.Red
         Me.AppVersion.Location = New System.Drawing.Point(105, 12)
         Me.AppVersion.Name = "AppVersion"
         Me.AppVersion.Size = New System.Drawing.Size(39, 13)
@@ -43,7 +47,9 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(181, Byte), Integer), CType(CType(229, Byte), Integer))
         Me.Label2.Location = New System.Drawing.Point(12, 12)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(63, 13)
@@ -53,7 +59,9 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(181, Byte), Integer), CType(CType(229, Byte), Integer))
         Me.Label3.Location = New System.Drawing.Point(12, 31)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(87, 13)
@@ -63,6 +71,8 @@ Partial Class Form1
         'UpdateVersion
         '
         Me.UpdateVersion.AutoSize = True
+        Me.UpdateVersion.BackColor = System.Drawing.Color.Transparent
+        Me.UpdateVersion.ForeColor = System.Drawing.Color.Lime
         Me.UpdateVersion.Location = New System.Drawing.Point(105, 31)
         Me.UpdateVersion.Name = "UpdateVersion"
         Me.UpdateVersion.Size = New System.Drawing.Size(39, 13)
@@ -71,40 +81,66 @@ Partial Class Form1
         '
         'Button1
         '
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.Red
         Me.Button1.Location = New System.Drawing.Point(255, 12)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 32)
         Me.Button1.TabIndex = 3
         Me.Button1.Text = "Abbrechen"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.UseVisualStyleBackColor = False
         '
         'Button2
         '
+        Me.Button2.BackColor = System.Drawing.Color.Transparent
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Lime
+        Me.Button2.Image = Global.Gingolingoo_Updater.My.Resources.Resources.download
+        Me.Button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
         Me.Button2.Location = New System.Drawing.Point(174, 12)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 32)
         Me.Button2.TabIndex = 4
         Me.Button2.Text = "Update"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Updateprogress
+        '
+        Me.Updateprogress.BackColor = System.Drawing.Color.FromArgb(CType(CType(16, Byte), Integer), CType(CType(16, Byte), Integer), CType(CType(16, Byte), Integer))
+        Me.Updateprogress.ForeColor = System.Drawing.Color.Gray
+        Me.Updateprogress.Location = New System.Drawing.Point(174, 12)
+        Me.Updateprogress.Name = "Updateprogress"
+        Me.Updateprogress.Size = New System.Drawing.Size(75, 32)
+        Me.Updateprogress.TabIndex = 5
+        Me.Updateprogress.Visible = False
+        '
+        'UpdateBW
+        '
+        Me.UpdateBW.WorkerReportsProgress = True
+        Me.UpdateBW.WorkerSupportsCancellation = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(342, 56)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(16, Byte), Integer), CType(CType(16, Byte), Integer), CType(CType(16, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(342, 55)
         Me.ControlBox = False
-        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.UpdateVersion)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.AppVersion)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Updateprogress)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Updater"
+        Me.Text = "Gingolingoo Updater"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -116,4 +152,6 @@ Partial Class Form1
     Friend WithEvents UpdateVersion As Label
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
+    Friend WithEvents Updateprogress As ProgressBar
+    Friend WithEvents UpdateBW As System.ComponentModel.BackgroundWorker
 End Class
--- Gingolingoo Updater/Form1.vb
+++ Gingolingoo Updater/Form1.vb
@@ -1,14 +1,18 @@
-﻿Imports System.IO
-Imports System.IO.Compression
+﻿Imports System.ComponentModel
+Imports System.IO
 Imports System.Net
 
 Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Dim filename As String
+    Dim WithEvents Client2 As WebClient = New WebClient()
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Control.CheckForIllegalCrossThreadCalls = False
         Dim address As String = "https://www.gingolingoo.de/content/GameLauncherVersion"
         Dim client As WebClient = New WebClient()
         Dim reader As StreamReader = New StreamReader(client.OpenRead(address))
         UpdateVersion.Text = reader.ReadToEnd
+        filename = "GGL_" & UpdateVersion.Text & "_Setup.exe"
         Try
             AppVersion.Text = GetFileVersionInfo(Application.StartupPath & "\Gingolingoo Launcher.exe").ToString
             If UpdateVersion.Text <= AppVersion.Text Then
@@ -26,42 +30,81 @@ Public Class Form1
         Try
             Return Version.Parse(FileVersionInfo.GetVersionInfo(filename).FileVersion)
         Catch
+            Return Nothing
         End Try
-        Return Nothing
     End Function
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            UpdateBW.CancelAsync()
+        Catch ex As Exception
+        End Try
         Application.Exit()
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Try
-            Dim address As String = "https://www.gingolingoo.de/content/"
-            Dim client As WebClient = New WebClient()
-            Dim filename As String = "GGL_" & UpdateVersion.Text & ".zip"
-            client.DownloadFile(New Uri(address & filename), Application.StartupPath & "\" & filename)
-            Button2.Enabled = False
-            Button2.Text = "Arbeitet..."
-            My.Computer.FileSystem.RenameFile(Application.StartupPath & "\Updater.exe", "Updater_old.exe")
-            Threading.Thread.Sleep(2)
-            UnZip()
-            Process.Start(Application.StartupPath & "\Gingolingoo Launcher.exe")
-            Application.Exit()
+            Updateprogress.Visible = True
+            Button2.Visible = False
+            UpdateApp()
         Catch ex As Exception
-            MsgBox(ex.ToString)
-            My.Computer.FileSystem.RenameFile(Application.StartupPath & "\Updater_old.exe", "Updater.exe")
+            Dim res As Long
+            res = MsgBox("Es ist ein Fehler aufgetreten. Möchtest du den Fehlercode sehen?", vbYesNo, "Fehler")
+            Select Case res
+                Case vbYes
+                    MsgBox(ex.ToString)
+                Case vbNo
+                    'nix
+            End Select
             Application.Exit()
         End Try
     End Sub
-End Class
-
-Module Module1
-    Sub UnZip()
-        Dim filename As String = "GGL_" & Form1.UpdateVersion.Text & ".zip"
-        Dim sc As New Shell32.Shell()
-        Dim output As Shell32.Folder = sc.NameSpace(Application.StartupPath & "\")
-        Dim input As Shell32.Folder = sc.NameSpace(Application.StartupPath & "\" & filename)
-        output.CopyHere(input.Items, 4)
-        File.Delete(Application.StartupPath & "\" & filename)
+    Private Sub Client2_DownloadProgressChanged(ByVal sender As Object, ByVal e As DownloadProgressChangedEventArgs)
+        Dim bytesIn As Double = Double.Parse(e.BytesReceived.ToString())
+        Dim totalBytes As Double = Double.Parse(e.TotalBytesToReceive.ToString())
+        Dim percentage As Double = bytesIn / totalBytes * 100
+        Me.Text = "Gingolingoo Updater - " & Int32.Parse(Math.Truncate(percentage).ToString()) & "%"
+        Updateprogress.Value = Int32.Parse(Math.Truncate(percentage).ToString())
     End Sub
-End Module
+
+    Public Function UpdateApp()
+        Try
+            AddHandler Client2.DownloadProgressChanged, AddressOf Client2_DownloadProgressChanged
+            AddHandler Client2.DownloadFileCompleted, AddressOf Client2_DownloadFileCompleted
+            Dim address As String = "https://www.gingolingoo.de/content/"
+            Try
+                Client2.DownloadFileAsync(New Uri(address & filename), Application.StartupPath & "\" & filename)
+            Catch
+            End Try
+        Catch ex As Exception
+            Dim res As Long
+            res = MsgBox("Es ist ein Fehler aufgetreten. Möchtest du den Fehlercode sehen?", vbYesNo, "Fehler")
+            Select Case res
+                Case vbYes
+                    MsgBox(ex.ToString)
+                Case vbNo
+                    'nix
+            End Select
+        End Try
+        Return False
+    End Function
+
+    Private Sub Client2_DownloadFileCompleted(sender As Object, e As AsyncCompletedEventArgs)
+        Updateprogress.Visible = False
+        Button2.Visible = True
+        Me.Text = "Gingolingoo Updater"
+        Try
+            Application.Exit()
+            Process.Start(Application.StartupPath & "\" & filename)
+        Catch exc As Exception
+            Dim res As Long
+            res = MsgBox("Es ist ein Fehler aufgetreten. Möchtest du den Fehlercode sehen?", vbYesNo, "Fehler")
+            Select Case res
+                Case vbYes
+                    MsgBox(exc.ToString)
+                Case vbNo
+                    'nix
+            End Select
+        End Try
+    End Sub
+End Class
